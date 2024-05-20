c38a871a4815d95cb884883e61e3d60dd1ede7a9
Austinlp4 <apendergrast44@gmail.com>
2024-05-20 17:00:33 -0500

    create account sheet and initial new account form

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin", "cyrillic-ext"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <html lang="en">
           <body className={inter.className}>
             <QueryProvider>
+              <SheetProvider />
               {children}
             </QueryProvider>
           </body>
